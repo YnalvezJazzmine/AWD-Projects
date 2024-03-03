5d465cce232b6351467ff483fb7c657e8f735ba9
Jazzmine Llana Ynalvez <157238959+YnalvezJazzmine@users.noreply.github.com>
2024-03-03 17:37:37 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var clearButton = document.getElementById('clear');
-    var answerField = document.forms['form1']['answer'];
-
-    clearButton.addEventListener('click', function () {
-        answerField.value = '';
-    });
-});
